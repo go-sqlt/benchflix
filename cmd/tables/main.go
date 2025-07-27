9a1da0ccc7bbb2b467348201b52a1ce95acd36df
Malte Wrogemann <wroge.malte@gmail.com>
2025-07-27 19:30:56 +0200

    show median delta

--- cmd/tables/main.go
+++ cmd/tables/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"math"
 	"os"
+	"reflect"
 	"strings"
 
 	"github.com/go-sqlt/benchflix"
@@ -14,53 +15,76 @@ import (
 func main() {
 	b := benchflix.Must(benchflix.ReadAll(os.Stdin))
 
-	PrintNsPerOp("SQL", b.SQL)
-	PrintNsPerOp("PGX", b.PGX)
-	PrintNsPerOp("SQUIRREL", b.SQUIRREL)
-	PrintNsPerOp("SQLX", b.SQLX)
-	PrintNsPerOp("GORM", b.GORM)
-	PrintNsPerOp("SQLC", b.SQLC)
-	PrintNsPerOp("SQLT", b.SQLT)
-	PrintNsPerOp("SQLT-Cache", b.SQLTCACHE)
+	NsPerOp("SQL", b.SQL, benchflix.Framework{})
+	NsPerOp("PGX", b.PGX, b.SQL)
+	NsPerOp("SQUIRREL", b.SQUIRREL, b.SQL)
+	NsPerOp("SQLX", b.SQLX, b.SQL)
+	NsPerOp("GORM", b.GORM, b.SQL)
+	NsPerOp("SQLC", b.SQLC, b.SQL)
+	NsPerOp("SQLT", b.SQLT, b.SQL)
+	NsPerOp("SQLT-Cache", b.SQLTCACHE, b.SQL)
 
-	PrintBytesPerOp("SQL", b.SQL)
-	PrintBytesPerOp("PGX", b.PGX)
-	PrintBytesPerOp("SQUIRREL", b.SQUIRREL)
-	PrintBytesPerOp("SQLX", b.SQLX)
-	PrintBytesPerOp("GORM", b.GORM)
-	PrintBytesPerOp("SQLC", b.SQLC)
-	PrintBytesPerOp("SQLT", b.SQLT)
-	PrintBytesPerOp("SQLT-Cache", b.SQLTCACHE)
+	BytesPerOp("SQL", b.SQL, benchflix.Framework{})
+	BytesPerOp("PGX", b.PGX, b.SQL)
+	BytesPerOp("SQUIRREL", b.SQUIRREL, b.SQL)
+	BytesPerOp("SQLX", b.SQLX, b.SQL)
+	BytesPerOp("GORM", b.GORM, b.SQL)
+	BytesPerOp("SQLC", b.SQLC, b.SQL)
+	BytesPerOp("SQLT", b.SQLT, b.SQL)
+	BytesPerOp("SQLT-Cache", b.SQLTCACHE, b.SQL)
 
-	PrintAllocsPerOp("SQL", b.SQL)
-	PrintAllocsPerOp("PGX", b.PGX)
-	PrintAllocsPerOp("SQUIRREL", b.SQUIRREL)
-	PrintAllocsPerOp("SQLX", b.SQLX)
-	PrintAllocsPerOp("GORM", b.GORM)
-	PrintAllocsPerOp("SQLC", b.SQLC)
-	PrintAllocsPerOp("SQLT", b.SQLT)
-	PrintAllocsPerOp("SQLT-Cache", b.SQLTCACHE)
+	AllocsPerOp("SQL", b.SQL, benchflix.Framework{})
+	AllocsPerOp("PGX", b.PGX, b.SQL)
+	AllocsPerOp("SQUIRREL", b.SQUIRREL, b.SQL)
+	AllocsPerOp("SQLX", b.SQLX, b.SQL)
+	AllocsPerOp("GORM", b.GORM, b.SQL)
+	AllocsPerOp("SQLC", b.SQLC, b.SQL)
+	AllocsPerOp("SQLT", b.SQLT, b.SQL)
+	AllocsPerOp("SQLT-Cache", b.SQLTCACHE, b.SQL)
 }
 
-func PrintNsPerOp(name string, f benchflix.Framework) {
+func NsPerOp(name string, framework benchflix.Framework, base benchflix.Framework) {
 	file := benchflix.Must(os.Create(fmt.Sprintf("data/%s_nsperop.tex", strings.ToLower(name))))
 
-	fmt.Fprintf(file, `\begin{table}[ht]
+	delta := fmt.Sprintf(`& ${\Delta M_{%s,SQL}}$`, name)
+	if reflect.DeepEqual(base, benchflix.Framework{}) {
+		delta = ""
+	}
+
+	fmt.Fprintf(file, `
+\begin{table}[ht]
 \centering
 \caption{%s: Nanosekunden pro Operation}
-\begin{tabular}{lrrrrrr}
+\begin{tabular}{lrrrr}
 \toprule
-Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
-\midrule`, name)
+Szenario & Params & ${M_{%s}}$ & ${QA_{%s}}$ %s  \\
+\midrule
+`, name, name, name, delta)
 
-	FprintParams(file, "List", "100", IgnoreErr(stats.Quartile(f.List.Hundred.NsPerOp)))
-	FprintParams(file, "ListPreload", "100", IgnoreErr(stats.Quartile(f.ListPreload.Hundred.NsPerOp)))
-	FprintParams(file, "Dashboard", "100", IgnoreErr(stats.Quartile(f.Dashboard.Hundred.NsPerOp)))
-	FprintParams(file, "DashboardPreload", "100", IgnoreErr(stats.Quartile(f.DashboardPreload.Hundred.NsPerOp)))
-	FprintParams(file, "List", "1000", IgnoreErr(stats.Quartile(f.List.Thousand.NsPerOp)))
-	FprintParams(file, "ListPreload", "1000", IgnoreErr(stats.Quartile(f.ListPreload.Thousand.NsPerOp)))
-	FprintParams(file, "Dashboard", "1000", IgnoreErr(stats.Quartile(f.Dashboard.Thousand.NsPerOp)))
-	FprintParams(file, "DashboardPreload", "1000", IgnoreErr(stats.Quartile(f.DashboardPreload.Thousand.NsPerOp)))
+	Print(file, "List", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Hundred.NsPerOp)
+	})
+	Print(file, "ListPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Hundred.NsPerOp)
+	})
+	Print(file, "Dashboard", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Hundred.NsPerOp)
+	})
+	Print(file, "DashboardPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Hundred.NsPerOp)
+	})
+	Print(file, "List", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Thousand.NsPerOp)
+	})
+	Print(file, "ListPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Thousand.NsPerOp)
+	})
+	Print(file, "Dashboard", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Thousand.NsPerOp)
+	})
+	Print(file, "DashboardPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Thousand.NsPerOp)
+	})
 
 	fmt.Fprintf(file, `
 \bottomrule
@@ -70,25 +94,48 @@ Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
 	`, strings.ToLower(name))
 }
 
-func PrintBytesPerOp(name string, f benchflix.Framework) {
+func BytesPerOp(name string, framework benchflix.Framework, base benchflix.Framework) {
 	file := benchflix.Must(os.Create(fmt.Sprintf("data/%s_bytesperop.tex", strings.ToLower(name))))
 
-	fmt.Fprintf(file, `\begin{table}[ht]
+	delta := fmt.Sprintf(`& ${\Delta M_{%s,SQL}}$`, name)
+	if reflect.DeepEqual(base, benchflix.Framework{}) {
+		delta = ""
+	}
+
+	fmt.Fprintf(file, `
+\begin{table}[ht]
 \centering
 \caption{%s: Speicherverbrauch pro Operation}
-\begin{tabular}{lrrrrrr}
+\begin{tabular}{lrrrr}
 \toprule
-Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
-\midrule`, name)
+Szenario & Params & ${M_{%s}}$ & ${QA_{%s}}$ %s \\
+\midrule
+`, name, name, name, delta)
 
-	FprintParams(file, "List", "100", IgnoreErr(stats.Quartile(f.List.Hundred.BytesPerOp)))
-	FprintParams(file, "ListPreload", "100", IgnoreErr(stats.Quartile(f.ListPreload.Hundred.BytesPerOp)))
-	FprintParams(file, "Dashboard", "100", IgnoreErr(stats.Quartile(f.Dashboard.Hundred.BytesPerOp)))
-	FprintParams(file, "DashboardPreload", "100", IgnoreErr(stats.Quartile(f.DashboardPreload.Hundred.BytesPerOp)))
-	FprintParams(file, "List", "1000", IgnoreErr(stats.Quartile(f.List.Thousand.BytesPerOp)))
-	FprintParams(file, "ListPreload", "1000", IgnoreErr(stats.Quartile(f.ListPreload.Thousand.BytesPerOp)))
-	FprintParams(file, "Dashboard", "1000", IgnoreErr(stats.Quartile(f.Dashboard.Thousand.BytesPerOp)))
-	FprintParams(file, "DashboardPreload", "1000", IgnoreErr(stats.Quartile(f.DashboardPreload.Thousand.BytesPerOp)))
+	Print(file, "List", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Hundred.BytesPerOp)
+	})
+	Print(file, "ListPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Hundred.BytesPerOp)
+	})
+	Print(file, "Dashboard", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Hundred.BytesPerOp)
+	})
+	Print(file, "DashboardPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Hundred.BytesPerOp)
+	})
+	Print(file, "List", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Thousand.BytesPerOp)
+	})
+	Print(file, "ListPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Thousand.BytesPerOp)
+	})
+	Print(file, "Dashboard", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Thousand.BytesPerOp)
+	})
+	Print(file, "DashboardPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Thousand.BytesPerOp)
+	})
 
 	fmt.Fprintf(file, `
 \bottomrule
@@ -98,25 +145,48 @@ Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
 	`, strings.ToLower(name))
 }
 
-func PrintAllocsPerOp(name string, f benchflix.Framework) {
+func AllocsPerOp(name string, framework benchflix.Framework, base benchflix.Framework) {
 	file := benchflix.Must(os.Create(fmt.Sprintf("data/%s_allocsperop.tex", strings.ToLower(name))))
 
-	fmt.Fprintf(file, `\begin{table}[ht]
+	delta := fmt.Sprintf(`& ${\Delta M_{%s,SQL}}$`, name)
+	if reflect.DeepEqual(base, benchflix.Framework{}) {
+		delta = ""
+	}
+
+	fmt.Fprintf(file, `
+\begin{table}[ht]
 \centering
 \caption{%s: Allokationen pro Operation}
-\begin{tabular}{lrrrrrr}
+\begin{tabular}{lrrrr}
 \toprule
-Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
-\midrule`, name)
+Szenario & Params & ${M_{%s}}$ & ${QA_{%s}}$ %s \\
+\midrule
+`, name, name, name, delta)
 
-	FprintParams(file, "List", "100", IgnoreErr(stats.Quartile(f.List.Hundred.AllocsPerOp)))
-	FprintParams(file, "ListPreload", "100", IgnoreErr(stats.Quartile(f.ListPreload.Hundred.AllocsPerOp)))
-	FprintParams(file, "Dashboard", "100", IgnoreErr(stats.Quartile(f.Dashboard.Hundred.AllocsPerOp)))
-	FprintParams(file, "DashboardPreload", "100", IgnoreErr(stats.Quartile(f.DashboardPreload.Hundred.AllocsPerOp)))
-	FprintParams(file, "List", "1000", IgnoreErr(stats.Quartile(f.List.Thousand.AllocsPerOp)))
-	FprintParams(file, "ListPreload", "1000", IgnoreErr(stats.Quartile(f.ListPreload.Thousand.AllocsPerOp)))
-	FprintParams(file, "Dashboard", "1000", IgnoreErr(stats.Quartile(f.Dashboard.Thousand.AllocsPerOp)))
-	FprintParams(file, "DashboardPreload", "1000", IgnoreErr(stats.Quartile(f.DashboardPreload.Thousand.AllocsPerOp)))
+	Print(file, "List", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Hundred.AllocsPerOp)
+	})
+	Print(file, "ListPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Hundred.AllocsPerOp)
+	})
+	Print(file, "Dashboard", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Hundred.AllocsPerOp)
+	})
+	Print(file, "DashboardPreload", "100", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Hundred.AllocsPerOp)
+	})
+	Print(file, "List", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.List.Thousand.AllocsPerOp)
+	})
+	Print(file, "ListPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.ListPreload.Thousand.AllocsPerOp)
+	})
+	Print(file, "Dashboard", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.Dashboard.Thousand.AllocsPerOp)
+	})
+	Print(file, "DashboardPreload", "1000", framework, base, func(f benchflix.Framework) (stats.Quartiles, error) {
+		return stats.Quartile(f.DashboardPreload.Thousand.AllocsPerOp)
+	})
 
 	fmt.Fprintf(file, `
 \bottomrule
@@ -126,16 +196,24 @@ Szenario & Params & Q1 & Q2 & Q3 & QA & QA/Q2 \\
 	`, strings.ToLower(name))
 }
 
-func IgnoreErr(q stats.Quartiles, err error) stats.Quartiles {
-	return q
-}
+func Print(w io.Writer, szenario string, params string, framework, base benchflix.Framework, fn func(benchflix.Framework) (stats.Quartiles, error)) {
+	f, err := fn(framework)
+	if err != nil {
+		return
+	}
 
-func FprintParams(w io.Writer, szenario string, params string, q stats.Quartiles) {
-	if q.Q1 == 0 {
+	b, err := fn(base)
+	if err != nil {
+		if err == stats.ErrEmptyInput {
+			fmt.Fprintf(w, `
+	%s & %s & %g & %g \\`,
+				szenario, params, math.Round(f.Q2), math.Round(f.Q3-f.Q1))
+		}
+
 		return
 	}
 
 	fmt.Fprintf(w, `
-		%s & %s & %g & %g & %g & %g & %.1f\%% \\`,
-		szenario, params, math.Round(q.Q1), math.Round(q.Q2), math.Round(q.Q3), math.Round(q.Q3-q.Q1), math.Round((q.Q3-q.Q1)/q.Q2*1000)/10)
+	%s & %s & %g & %g & %.1f\%% \\`,
+		szenario, params, math.Round(f.Q2), math.Round(f.Q3-f.Q1), math.Round(f.Q2/b.Q2*1000)/10-100)
 }
